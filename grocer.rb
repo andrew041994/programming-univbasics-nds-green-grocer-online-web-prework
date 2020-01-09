87a60e80f56aaa2866b640691963f0aa11830f6d
Alehandro Persaud <andrew041994@gmail.com>
2020-01-09 04:48:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   i=0
   while i<collection.size
     if collection[i][:item]==name
-      return collection[i]      
+      return collection[i]
     end
     i+=1
   end
